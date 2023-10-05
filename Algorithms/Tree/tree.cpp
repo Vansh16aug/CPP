50ccccec915a633b348f481846ee85fc990d04c2
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-05 13:56:40 +0530

    2

---
+++ Algorithms/Tree/tree.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        val=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+class Tree{
+    public:
+    Node* head;
+    Node* tail;
+
+    Tree(){
+        head=NULL;
+        tail=NULL;
+    }
+    
+};
+int main()
+{
+    
+    return 0;
+}
