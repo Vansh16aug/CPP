9db1e981581608ac88358c9807cedc9b8befe182
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-07 18:56:11 +0530

    2

--- Algorithms/Tree/tree.cpp
+++ Algorithms/Tree/tree.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 class Node{
     public:
@@ -12,11 +12,12 @@ class Node{
         right=NULL;
     }
 };
+
 Node* buildTree(){
     int data;
-    cout<<"Enter data for tree :";
+    // cout<<"Enter data for tree :";
     cin>>data;
-
+    cout<<endl;
     if(data==-1){
         return NULL;
     }
@@ -26,9 +27,45 @@ Node* buildTree(){
     return new_node;
 }
 
+// Preorder traversal (Root -> Left -> Right)
+void preOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->val<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+//Inorder traversal (Left -> Root -> Right)
+void inOrder(Node* root){
+    if (root == NULL)
+    return;
+    inOrder(root->left);
+    cout<<root->val<<" ";
+    inOrder(root->right);
+}
+
+// Postorder traversal (Left -> Right -> Root)
+void postOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->val<<" ";
+}
 int main()
 {
-    
+    // 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
     Node* root=buildTree();
+    cout<<"After doing Preorder Traversal :";
+    preOrder(root);
+    cout<<endl;
+    cout<<"After doing Inorder Traversal :";
+    inOrder(root);
+    cout<<endl;
+    cout<<"After doing Postorder Traversal :";
+    postOrder(root);
     return 0;
 }
