59ffef8b4f5013d8af7ee1a02d5ed67e1257b57c
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-08 17:45:35 +0530

    1

--- Algorithms/BinarySearchTree/bst.cpp
+++ Algorithms/BinarySearchTree/bst.cpp
@@ -24,6 +24,7 @@ Node* insert(Node* root,int key){
     return root;
 }
 
+
 void inOrder(Node* root){
     if(root==NULL){
         return;
@@ -32,6 +33,75 @@ void inOrder(Node* root){
     cout<<root->key<<" ";
     inOrder(root->right);
 }
+
+
+bool search(Node* root,int key){
+    if(root==NULL){
+        return false;
+    }
+    if(root->key==key){
+        return true;
+    }
+    if(key>root->key){
+        return search(root->left,key);
+    }
+    return search(root->right,key);
+}
+
+Node* findMin(Node* root){
+    while(root->left!=NULL){        //root->left coz left has small values than root
+        root=root->left;
+    }
+    return root;        // returns smallest root node
+}
+
+
+Node* remove(Node* root,int key){
+    if(root==NULL){
+        return NULL;
+    }
+    // To Find the Key 
+    else if(key<root->key){
+        root->left=remove(root->left,key);
+    }
+    else if(key>root->key){
+        root->right=remove(root->right,key);
+    }
+    else{
+        // When the Current Node Is Equal to Key
+
+        // No Children
+        if(root->left==NULL and root->right==NULL){
+            delete root;
+            root=NULL;
+        }
+        // One Childern
+        else if(root->left==NULL){
+            Node* temp=root;
+            root=root->right;
+            delete root;
+        }
+        else if(root->right==NULL){
+            Node* temp=root;
+            root=root->left;
+            delete root;
+        }
+        // Two Children
+        else{
+            Node*temp=findMin(root->right);     //finding inorder successor from right subtree
+            
+            root->key=temp->key;        // copying inorder successor to key node
+
+            root->right=remove(root->right,temp->key);      // removing temp->key (temp id inorder succesor ) and the whole subtree is to right of root
+        }
+
+    return root;
+
+    }
+
+}
+
+
 int main()
 {
     Node* root=NULL;
@@ -39,6 +109,33 @@ int main()
     for(int x:arr){     // traversing through array
         root=insert(root,x);
     }
+    cout<<"Inorder Traversal gives :";
     inOrder(root);
+    cout<<endl;
+
+    //Searching
+    int key;
+    cout<<"Enter what you want to search :";
+    cin>>key;
+    cout<<endl;
+    int ans=search(root,key);
+    if(ans==1){
+        cout<<"Key is present in Tree"<<endl;
+    }
+    else{
+        cout<<"Key is not present in Tree"<<endl;
+    }
+    cout<<endl;
+
+    //Deletion
+    int key2;
+    cout<<"Enter what you want to delete :";
+    cin>>key2;
+    cout<<endl;
+    root=remove(root,key2);
+    cout<<endl;
+    cout<<"After deletion :";
+    inOrder(root);
+    cout<<endl;
     return 0;
 }
