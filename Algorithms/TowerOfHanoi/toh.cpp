d7cb626e7f97cff1a5cbf6999117fc4cbdb136b0
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-08 19:11:31 +0530

    3

--- Algorithms/TowerOfHanoi/toh.cpp
+++ Algorithms/TowerOfHanoi/toh.cpp
@@ -1,14 +1,15 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-void TOH(int n,int A,int B,int C){  //A ->SOURCE    C->DESTINATION  
-    if(n>0){
-        TOH(n-1,A,C,B);
-        cout<<A<<" "<<C;
-        TOH(n-1,B,A,C);
+void TowerOfHanoi(int start,int end,char source,char auxillary,char destination){   //start and end are starting and ending number of disks
+    if(start>end){
+        return;
     }
+    TowerOfHanoi(start,end-1,source,destination,auxillary);
+    cout<<"Move disk "<<end<<" from "<<source<<" to "<<destination<<endl;
+    TowerOfHanoi(start,end-1,auxillary,source,destination);
 }
 int main()
 {
-    TOH(3,1,2,3);   //n A B C
+    TowerOfHanoi(1,3,'A','B','C');
     return 0;
 }
