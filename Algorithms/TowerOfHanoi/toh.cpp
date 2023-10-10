490f32189e491725a5ecf778f52d70a691894f92
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-10 12:25:19 +0530

    1

--- Algorithms/TowerOfHanoi/toh.cpp
+++ Algorithms/TowerOfHanoi/toh.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void TowerOfHanoi(int start,int end,char source,char auxillary,char destination){   //start and end are starting and ending number of disks
-    if(start>end){
+    if(start>end){      //if we have only one disk
         return;
     }
     TowerOfHanoi(start,end-1,source,destination,auxillary);
@@ -10,6 +10,6 @@ void TowerOfHanoi(int start,int end,char source,char auxillary,char destination)
 }
 int main()
 {
-    TowerOfHanoi(1,3,'A','B','C');
+    TowerOfHanoi(1,1,'A','B','C');
     return 0;
 }
