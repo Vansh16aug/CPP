6bdc968a3ae4a28a3313246e184160d77b4793e8
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-12 12:10:19 +0530

    6

--- Algorithms/MergeSort/ms.cpp
+++ Algorithms/MergeSort/ms.cpp
@@ -1,33 +1,32 @@
 #include<iostream>
 using namespace std;
 
-
-
+//merging
 void merge(int arr[], int beg, int mid, int end)    
 {    
     int i, j, k;  
-    int n1 = mid - beg + 1;    
-    int n2 = end - mid;    
+    int n1 = mid - beg + 1;    //size of the left subarray,
+    int n2 = end - mid;     //size of the right subarray
     int LeftArray[n1], RightArray[n2]; 
     for (int i = 0; i < n1; i++)    
     LeftArray[i] = arr[beg + i];    
     for (int j = 0; j < n2; j++)    
-    RightArray[j] = arr[mid + 1 + j]; 
-    i = 0; 
-    j = 0;   
-    k = beg;    
+        RightArray[j] = arr[mid + 1 + j]; 
+        i = 0; 
+        j = 0;   
+        k = beg;    
     while (i < n1 && j < n2)    
     {    
         if(LeftArray[i] < RightArray[j])    
-        {    
-            arr[k] = LeftArray[i];    
-            i++;    
-        } 
-else    
-        {    
-            arr[k] = RightArray[j];    
-            j++;    
-        }    
+            {    
+                arr[k] = LeftArray[i];    
+                i++;    
+            } 
+        else    
+            {    
+                arr[k] = RightArray[j];    
+                j++;    
+            }    
         k++;    
     }    
     while (i<n1)    
@@ -43,6 +42,8 @@ while (j<n2)
         k++;    
   }    
 }  
+
+//sorting algorithm
 void mergeSort(int arr[], int beg, int end)  
    {  
     if (beg < end)   
@@ -51,14 +52,13 @@ void mergeSort(int arr[], int beg, int end)
         mergeSort(arr, beg, mid);  
         mergeSort(arr, mid + 1, end);  
         merge(arr, beg, mid, end);  
-      }  
+    }  
 }
 
-void printArray(int arr[], int n)  
-    {  
+//printing the array
+void printArray(int arr[], int n){  
     int i;  
-    for (i = 0; i < n; i++)  
-        cout<<arr[i]<<" ";
+    for (i = 0; i < n; i++)  cout<<arr[i]<<" ";
 } 
 
 
