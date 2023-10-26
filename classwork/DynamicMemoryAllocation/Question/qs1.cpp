f8686be980e3dadde12b3d848a04bfce86b71fed
Vansh16aug <kumarvansh16aug@gmail.com>
2023-10-26 07:24:35 +0530

    1

--- classwork/DynamicMemoryAllocation/Question/qs1.cpp
+++ classwork/DynamicMemoryAllocation/Question/qs1.cpp
@@ -17,7 +17,7 @@ struct Student {
 
 int main() {
     int choice;
-    Student* students = nullptr; // Use nullptr instead of NULL
+    Student* students = nullptr; // this points to the records
     int numStudents = 0;
 
     while (true) {
@@ -35,15 +35,15 @@ int main() {
                 {
                     numStudents++;
                     Student* newStudents = new Student[numStudents];
-                    if (students) {
-                        for (int i = 0; i < numStudents - 1; i++) {
-                            newStudents[i] = students[i];
+                    if (students!=NULL) {       //checks if the pointer is null or not
+                        for (int i = 0; i < numStudents - 1; i++) {     // iterates till existing records as new is still not added
+                            newStudents[i] = students[i];       //copies the data from the existing students array to the new newStudents array.
                         }
-                        delete[] students;
+                        delete[] students;      // deletes data from old students array
                     }
-                    students = newStudents;
+                    students = newStudents;     // now students points the newly created memory
 
-                    Student& newStudent = students[numStudents - 1];
+                    Student& newStudent = students[numStudents - 1];        //refrence variable is accessing the last record
                     cout << "Enter name: ";
                     cin >> newStudent.name;
                     cout << "Enter age: ";
@@ -51,9 +51,9 @@ int main() {
                 }
                 break;
 
-            case 2: // Delete student
+            case 2: // Delete student   -> in this we create new array and store the records leaving or deleting the last record
                 {
-                    if (numStudents > 0) {
+                    if (numStudents > 0) {      // whether there is any record or not
                         numStudents--;
                         Student* newStudents = new Student[numStudents];
                         for (int i = 0; i < numStudents; i++) {
@@ -65,14 +65,14 @@ int main() {
                 }
                 break;
 
-            case 3: // Enter info of student
+            case 3: // Enter info of student / updater
                 {
                     int studentIndex;
                     cout << "Enter the index of the student to update: ";
                     cin >> studentIndex;
 
                     if (studentIndex >= 0 && studentIndex < numStudents) {
-                        Student& student = students[studentIndex];
+                        Student& student = students[studentIndex];  // reference variable pointing to array 
                         cout << "Enter new name: ";
                         cin >> student.name;
                         cout << "Enter new age: ";
