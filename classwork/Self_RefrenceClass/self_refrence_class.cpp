4070e9345f784cb27ef367379509376f4072b384
Vansh16aug <kumarvansh16aug@gmail.com>
2023-11-03 08:06:52 +0530

    l,o

--- classwork/Self_RefrenceClass/self_refrence_class.cpp
+++ classwork/Self_RefrenceClass/self_refrence_class.cpp
@@ -1,12 +1,50 @@
-#include<iostream>
-using namespace std;
-struct Student{
-    int n;
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
 };
-int main()
-{
-    Student s;
-    s.n=10;
-    cout<<s.n;
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void append(int data) {
+        Node* new_node = new Node(data);
+        if (!head) {
+            head = new_node;
+        }
+        else {
+            Node* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = new_node;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "NULL" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList myLinkedList;
+    myLinkedList.append(1);
+    myLinkedList.append(2);
+    myLinkedList.append(3);
+
+    myLinkedList.display();
+
     return 0;
 }
