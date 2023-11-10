5977940c7d630e1f1fcc92a01952e8f75dd8a098
Vansh16aug <kumarvansh16aug@gmail.com>
2023-11-10 05:46:38 +0530

    d

---
+++ STL_CLASS/list/list.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int>gqlist{12,45,8,6};
+    for(auto i:gqlist){
+        cout<<i<<' ';
+    }
+    return 0;
+}
---
+++ STL_CLASS/list/list_queue.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int>l;
+    int a;
+    cin>>a;
+    int num;
+    for(auto i=0;i<a;i++){      //1 2 3 4 5
+        cin>>num;
+        l.push_back(num);
+    }
+    for(auto i:l){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    l.pop_front();
+    l.pop_front();
+    
+    cout<<"After Popping :"<<endl;
+    for(auto i:l){
+        cout<<i<<" ";
+    }
+    return 0;
+}
