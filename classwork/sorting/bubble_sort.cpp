025a8f716c46278026e5d583b021cce89960c7b7
Vansh16aug <kumarvansh16aug@gmail.com>
2023-11-20 21:15:00 +0530

    tt

--- classwork/sorting/bubble_sort.cpp
+++ classwork/sorting/bubble_sort.cpp
@@ -4,10 +4,10 @@ void bubble_sort(int arr[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
-                // swap(arr[j],arr[j+1]);
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                swap(arr[j],arr[j+1]);
+                // int temp=arr[j];
+                // arr[j]=arr[j+1];
+                // arr[j+1]=temp;
             }
         }
     }
@@ -22,7 +22,17 @@ int main()
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    bubble_sort(arr,n);
+    // bubble_sort(arr,n);
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                // int temp=arr[j];
+                // arr[j]=arr[j+1];
+                // arr[j+1]=temp;
+            }
+        }
+    }
     cout<<"The sorted array is :";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
