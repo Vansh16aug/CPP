f9e8ac3860866658208307d66b46b792bd1e1906
Your Name <your.email@example.com>
2024-10-23 00:08:18 +0530

    stack reversing

--- Recursion/Questions/AllInOneRecursion.cpp
+++ Recursion/Questions/AllInOneRecursion.cpp
@@ -247,11 +247,45 @@
 //     return 0;
 // }
 
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    
-    return 0;
-}
+// #include <bits/stdc++.h>
+// using namespace std;
+// void insertAtBottom(stack<int> &st, int ele)
+// {
+//     if (st.empty())
+//     {
+//         st.push(ele);
+//         return;
+//     }
+//     int top = st.top();
+//     st.pop();
+//     insertAtBottom(st, ele);
+//     st.push(top);
+// }
+// void reverse(stack<int> &st)
+// {
+//     if (st.empty())
+//     {
+//         return;
+//     }
+//     int top = st.top();
+//     st.pop();
+//     reverse(st);
+//     insertAtBottom(st, top);
+// }
+// int main()
+// {
+//     stack<int> st;
+//     st.push(1);
+//     st.push(2);
+//     st.push(3);
+//     st.push(4);
+//     reverse(st);
+//     while (!st.empty())
+//     {
+//         cout << st.top() << " ";
+//         st.pop();
+//     }
+//     cout << endl;
+//     return 0;
+// }
 
